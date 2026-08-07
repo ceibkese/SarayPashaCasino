487e9f7fcba8be85a0ae269971839b045214be4c
ceibkese <ceibkese@gmail.com>
2026-08-08 00:21:18 +0300

    Remove link redirection and add alert
    Removed click event listener for specific link redirection and added an alert.

--- custom.js
+++ custom.js
@@ -1,8 +1,1 @@
-document.addEventListener("click", function (e) {
-    const link = e.target.closest('a[href="/tr/?m=account&t=instant_cashback"]');
-
-    if (link) {
-        e.preventDefault();
-        window.location.href = "https://www.saraypasha.com";
-    }
-});
+alert("custom.js çalıştı");
