9b9bb1354627e274cff25d0f8a033c77cab38101
ceibkese <ceibkese@gmail.com>
2026-08-14 14:46:15 +0300

    Add custom JavaScript for SarayPasha

---
+++ saraypasha-custom.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+
+  if (window.__sarayPashaCustomLoaded) {
+    return;
+  }
+
+  window.__sarayPashaCustomLoaded = true;
+  console.info("SarayPasha özel JavaScript başarıyla yüklendi.");
+})();
